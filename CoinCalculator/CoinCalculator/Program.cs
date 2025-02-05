180255584bae5ab098434ff59bdf052d9904165d
Ingvar Derešivski <inde999@gmail.com>
2025-02-05 14:58:22 +0200

    CoinCalculator

---
+++ CoinCalculator/CoinCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CoinCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Raha ümber maa!");
+
+            //vaja arvutada, mitu kahe eurost münti läheb ümber maa
+            //maa ümbermõõt tuleb välja arvutada raadiuse abil
+
+            double coinDiameter = 25.75; //mm
+            double earthRadius = 6378000000;
+            double aroundEarth = 2 * Math.PI * earthRadius;
+
+            Console.WriteLine("Maa ümbermõõt: " + aroundEarth);
+
+            //nüüd arvutame müntide arvu
+            double result = aroundEarth / coinDiameter;
+
+            Console.WriteLine("Ümber maa mahub nii " +
+                "palju kahe euroseid " + result);
+        }
+    }
+}
